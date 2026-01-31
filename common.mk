5f5c5c1f150d6a79e644fe7a057b85b412758167
anky894 <ko282ko82@gmail.com>
2026-02-01 03:42:04 +0530

    sm8650-common: Adapt powerhint for cliffs
    Co-authored-by: inferno0230 <mail@inferno0230.in>
    Change-Id: Iba2171444ebd19c00fa51b6b609856c04ef3f74b

--- common.mk
+++ common.mk
@@ -328,6 +328,9 @@ PRODUCT_PACKAGES += \
     android.hardware.power-service.lineage-libperfmgr \
     libqti-perfd-client
 
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/perf/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json
+
 # QTI fwk-detect
 PRODUCT_PACKAGES += \
     libvndfwk_detect_jni.qti_vendor # Needed by CNE app
